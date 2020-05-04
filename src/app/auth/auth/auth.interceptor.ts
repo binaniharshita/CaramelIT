632ccf18f1a75f2e250291f6c623b8c948d96b1e
Shrist Das <56693797+ShristDas@users.noreply.github.com>
2020-05-05 02:30:48 +0530

    Add files via upload

---
+++ src/app/auth/auth/auth.interceptor.ts
@@ -0,0 +1,45 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Router } from "@angular/router";
+
+import { StudentUserService } from '../shared-user/student/student-user.service';
+import { InstructorUserService } from '../shared-user/instructor/instructor-user.service';
+import { CorporateUserService } from '../shared-user/corporate/corporate-user.service';
+import { UniversityUserService } from '../shared-user/university/university-user.service';
+
+
+
+@Injectable() 
+export class AuthInterceptor implements HttpInterceptor{
+
+    constructor(private studentUserService: StudentUserService, 
+        private instructorUserService: InstructorUserService,
+        private corporateUserService: CorporateUserService,
+        private universityUserService: UniversityUserService,
+        private router: Router) {}
+
+
+    intercept(req: HttpRequest<any>, next: HttpHandler){
+        if(req.headers.get('noauth'))
+            return next.handle(req.clone());
+        else {
+            const clonereq = req.clone({
+                headers: (req.headers.set("Authorization", " Bearer " + this.studentUserService.getToken())||
+                req.headers.set("Authorization", " Bearer " + this.instructorUserService.getToken())||
+                req.headers.set("Authorization", " Bearer " + this.corporateUserService.getToken())||
+                req.headers.set("Authorization", " Bearer " + this.universityUserService.getToken()))
+            });
+            return next.handle(clonereq).pipe(
+                tap(
+                    event => { },
+                    err => {
+                        if(err.error.auth == false) 
+                            this.router.navigateByUrl('');
+                    }
+                )
+            );
+
+        }
+    }
+}
