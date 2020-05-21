1c65f5f89a8a7b49e03a625c473bc7135d1e937b
navin <navinsharmastudies@gmail.com>
2020-05-21 14:02:38 +0530

    academy page

--- src/app/academypage/ourprogram/ourprogram.component.ts
+++ src/app/academypage/ourprogram/ourprogram.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Course } from '../../shared/course';
 import { CourseService } from '../../services/course.service';
+import { CategoryService } from '../../admin-dashboard/manage-category/category.service';
+import { Category } from '../../admin-dashboard/manage-category/category.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ourprogram',
@@ -12,10 +15,15 @@ export class OurprogramComponent implements OnInit {
 
   divShow = false;
   courses: Course[];
-  constructor(private courseService: CourseService) { }
+  categories: Category[];
+  private categorySubs: Subscription;
+  constructor(private courseService: CourseService, private categoriesService: CategoryService) { }
 
   ngOnInit(): void {
     this.courses = this.courseService.getCourses();
+    this.categoriesService.getCategories();
+    this.categorySubs = this.categoriesService.getCategoriesUpdateListener()
+    .subscribe((categories: Category[]) => { this.categories = categories; });
   }
   onSelect(course: Course) {
     this.courseService.displayCourse(course);
