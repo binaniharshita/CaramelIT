8b074e8d03c787dcb198564c1ccbc87ab05ce797
rohitopen-source <rohit93199@gmail.com>
2020-06-05 13:05:00 +0530

    table of content rendered

--- src/app/course-page/course-content-display/course-content-display.component.ts
+++ src/app/course-page/course-content-display/course-content-display.component.ts
@@ -8,12 +8,12 @@ import { Course } from '../../admin-dashboard/manage-course/course.model';
 })
 export class CourseContentDisplayComponent implements OnInit {
   courseName;
-  public courseDiscription;
+  //public courseDiscription;
 
   @Input()
   course: Course;
 
-  lectures:string;
+  public lectures='123';
   // courseName: string;
   // lectures: string[] = ["Topic 1", "Topic 2", "Topic 3", "Topic 4", "Topic 5", "Topic 6", "Topic 7", "Topic 8", "Topic 9","Topic 10", "Topic 11",
   // "Topic 12", "Topic 13","Topic 14","Topic 15","Topic 16", "Topic 17", "Topic 18","Topic 19"];
@@ -25,10 +25,10 @@ export class CourseContentDisplayComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.course.contentModule);
     this.courseName = this.course.title;
-    this.courseDiscription = this.course.description;
+    //this.courseDiscription = this.course.contentModule[1];
 
-
-    this.lectures = this.course.contentModule[2];
+     document.getElementById("tableCon").innerHTML = JSON.stringify(this.course.contentModule,null,4);
+    //this.lectures = this.course.contentModule[0][3][1];
     console.log(this.lectures);
 
   }
